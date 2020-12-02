eed84a5e6f9c4e3d9723d7471496058543012111
ambergon <foxgon@outlook.jp>
2020-12-03 08:04:41 +0900

    [change]
    VISUAL MODEによる複数行選択に対応

--- autoload/a_vim_markdown_link.py
+++ autoload/a_vim_markdown_link.py
@@ -7,23 +7,38 @@ import re
 import sys
 
 line = vim.current.line
-#line = 'aaa[](https://ambergonslibrary.com/)'
-#line = 'aaa[aaa](https://ambergonslibrary.com/)'
 
-r = r'(\[(?P<title>.*?)\](?P<url>\(http.+?\)))'
-p = re.compile(r)
-m = p.search(line)
-if m == None:
-    #matchしなかった場合
-    print('NO URL')
-    #sys.stderr.write('NO URL')
-elif m.group('title') == "" :
-    #titleが挿入されていない場合のみ
-    url = m.group('url').replace('(','').replace(')','')
-      
-    html = urllib.urlopen(url)
-    soup = BeautifulSoup(html,"html.parser")
-    text = soup.title.string
-    title_text = text.encode('utf-8')
+line_parts = re.split(r'(\[.*?\]\(http.*?\))',line)
+result = ""
+for line_str in  line_parts :
+    if line_str == "":
+        continue
 
-    vim.current.line = re.sub(r'\[\]','[' + title_text + ']',line)
+    r = r'\[(?P<title>)\](?P<url>\(http.+?\))'
+    p = re.compile(r)
+    m = p.search(line_str)
+    
+    if m == None:
+        #matchしなかった場合
+        #print('NO URL')
+        #sys.stderr.write('NO URL')
+        result = result + line_str
+        continue
+    elif m.group('title') == "" :
+        #titleが挿入されていない場合のみ
+        url = m.group('url').replace('(','').replace(')','')
+
+        html = urllib.urlopen(url)
+        soup = BeautifulSoup(html,"html.parser")
+        text = soup.title.string
+        title_text = text.encode('utf-8')
+
+        xxx = re.sub(r'\[\]','[' + title_text + ']',line_str ,count=1)
+        result = result + xxx
+
+    else :
+        result = result + line_str
+        
+#print result
+vim.current.line = result
+
--- autoload/a_vim_markdown_link.vim
+++ autoload/a_vim_markdown_link.vim
@@ -2,7 +2,10 @@ let s:dir = expand('<sfile>:p:h')
 let s:py_path = s:dir . '/' . 'a_vim_markdown_link.py'
 "pyfileを指定する際にvimの変数は使えない？
 
-function! a_vim_markdown_link#get() abort
-    execute ':pyfile ' . s:py_path
+function! a_vim_markdown_link#get(...) abort
+    for n in range(a:1, a:2)
+        call cursor( n , 0 )
+        execute ':pyfile ' . s:py_path
+    endfor
 endfunction
 
